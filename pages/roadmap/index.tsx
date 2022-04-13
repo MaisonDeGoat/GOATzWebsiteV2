bb6ac35a567e8e543dd1624d83806d2460a5673e
Bryan Rhodes <85260508+MaisonDeGoat@users.noreply.github.com>
2022-04-13 00:24:42 -0700

    Update index.tsx

--- pages/roadmap/index.tsx
+++ pages/roadmap/index.tsx
@@ -13,36 +13,67 @@ const RoadMap = () => {
         <Image src={RoadMapCover} alt="goatz" layout="responsive" />
         <p className={style.content}>
           Our Mission for Roadmap 2.0  is Community + Gamification
+          <br/>
           Our Mantra that we continue to live by is Organic, Sustainable and Meaningful Growth.
+          <br/>
           We have listened to the community and have aligned our roadmap with what you see is valuable to GOATz and where we need to allocate our budget and effort. 
+          <br/>
           <b>$GMILK</b>
+          <br/>
           $GMILK is short for GOATzMILK and our in-game utility token for the GOATz Universe! 
+          <br/>
           1 GMILK = 1 GMILK
+          <br/>
           How you will obtain GMILK is through staking which brings us to another bottle, The KIDz Academy. Here you will drop off your KIDz at the academy through a staking contract that will allow you to gain GMILK over time staked. You can find our whitepaper here.
+          <br/>
           GMILK will become the utility currency in which things are purchased in the GOATz Universe. You will be able to personalize and name your GOATz going forward for a small GMILK fee. You’ll be able to unlock AUT’d /unforgeable GOATz with GMILK. And a major one for many, You’ll be able to combine your properties in GOATzVILLE to gain some pretty awesome upgrades. This is just scratching the surface of what can be done with GMILK and we will continue to add utility to this token.
+          <br/>
           <b>Discord Upgrade </b>
+          <br/>
           Because Discord is the current home for our community, we will be investing some time and effort in upgrading our Discord interactivity and security.
+          <br/>
           <b>New Website </b>
+          <br/>
           As we continue to add utility and add to our modular ecosystem it is necessary that our website makes it easy for owners to navigate and do what they want to do.
+          <br/>
           <b>The Forge</b>
+          <br/>
           Our identity is centered around The Forge and that will continue going forward as we add new innovative incentives for holders.
+          <br/>
           <b>GOATz X</b>
+          <br/>
           GOATz X is about creating partnerships and collaborations this year. Going forward there will be a focus on creating meaningful partnerships and collaborating across different IPs. We will be allocating dedicated resources and it will be a personal focus area of mine to lock in and activate these partnerships. We have already established partnerships with VHS, NiftyGateway, and Sandbox.
+          <br/>
           <b>IRL Events</b>
+          <br/>
           We are currently planning on an event at NFT NYC in June. If 2 or more GOATz are gathering, we would love to support it, financially and with our presence. Let us know what you have planned.
+          <br/>
           <b>GOATzVILLE</b>
+          <br/>
           GOATzVILLE is our 6x6 estate in the Sandbox near many other projects like BAYC, ZED RUN, and CyberKongs. It will contain our Maison De GOAT, the exclusive clubhouse mansion for GOATz as well as the 1000+ houses built in honor of all of our actively Forged GOATz. We will be launching as soon as Sandbox opens up their Public Beta release.
+          <br/>
           <b>Downtown</b>
+          <br/>
           We have decided to dedicate part of our Sandbox land specifically a 2x1 portion, to a GOATz Downtown which will include things like a fire station, school, and hospital. We are currently working on this build and can’t wait to share more!
+          <br/>
           <b>New Merch</b>
+          <br/>
           We will continue to rotate products in and out and keep the brand fresh and always moving. You can find our merch here.
+          <br/>
           <b>GOATz University</b>
-          We will be providing materials in the form of infographics, youtube videos, and even branching out to new mediums of social media for us. In addition we recognize for a lot of new GOATz it might be their first NFT they ever buy. Providing resources for first time people jumping into NFTs is also important to us. 
+          <br/>
+          We will be providing materials in the form of infographics, youtube videos, and even branching out to new mediums of social media for us. In addition we recognize for a lot of new GOATz it might be their first NFT they ever buy. Providing resources for first time people jumping into NFTs is also important to us.
+          <br/> 
           <b>Adventure Land</b>
+          <br/>
           It is a metaverse exclusive to KIDz owners where you will be able to compete against other KIDz in mini games themed around a carnival/theme park for prizes. Adventure land is being built in NFT worlds currently.
+          <br/>
           <b>Legacy Challenge</b>
+          <br/>
           A new ongoing series of challenges where owners must have certain matching traits between their GOATz and KIDz to enter.
+          <br/>
           Community + Gamification will be the cornerstones of the GOATz Universe going forward.
+
 
         </p>
       </Container>
